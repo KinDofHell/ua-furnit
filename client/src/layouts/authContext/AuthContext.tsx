7b3e6e08b09fa8fb6a6ddee32ecb28148934fe27
Ihor Dzhus <wildghost24@gmail.com>
2023-06-20 19:32:37 +0300

    Fix auth

--- client/src/layouts/authContext/AuthContext.tsx
+++ client/src/layouts/authContext/AuthContext.tsx
@@ -1,55 +1,54 @@
-import { createContext, useState, ReactNode, FC, useEffect } from "react";
+import {createContext, useState, ReactNode, FC, useEffect} from "react";
 import axiosInstance from "../../utils/axiosInstance";
 
 interface AuthProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 interface AuthContextValue {
-  isAuthenticated: boolean;
-  login: (login: string | null, password: string | null) => Promise<void>;
-  logout: () => void;
+    isAuthenticated: boolean;
+    login: (login: string | null, password: string | null) => Promise<void>;
+    logout: () => void;
 }
+
 export const AuthContext = createContext<AuthContextValue>(null!);
 
-export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [token, setToken] = useState<string>("");
+export const AuthProvider: FC<AuthProviderProps> = ({children}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [token, setToken] = useState<boolean>(false);
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    if (storedToken) {
-      setIsAuthenticated(true);
-    }
-  }, []);
+    useEffect(() => {
+        setToken(!!localStorage.getItem("token"));
+        if (token)
+            setIsAuthenticated(true);
+    }, [token]);
 
-  const login = async (login: string | null, password: string | null) => {
-    try {
-      await axiosInstance
-        .post("/login", {
-          login: login,
-          password: password,
-        })
-        .then((res) => {
-          token &&
-            !localStorage.getItem("token") &&
-            localStorage.setItem("token", res.data);
-          setIsAuthenticated(true);
-        });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const login = async (login: string | null, password: string | null) => {
+        try {
+            await axiosInstance
+                .post("/login", {
+                    login: login,
+                    password: password,
+                })
+                .then((res) => {
+                    localStorage.setItem("token", res.data);
+                    setIsAuthenticated(true);
+                });
+        } catch (error) {
+            alert("Помилка авторизації: пароль чи логін невірні!")
+        }
+    };
 
-  const logout = () => {
-    if (window.confirm("Ви дійсно хочете вийти?"))
-      localStorage.removeItem("token");
-    setIsAuthenticated(false);
-  };
+    const logout = () => {
+        if (window.confirm("Ви дійсно хочете вийти?")) {
+            localStorage.removeItem("token");
+            setIsAuthenticated(false);
+        }
+    };
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{isAuthenticated, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
